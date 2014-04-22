feb29fc8ef9a9e11c69b2395693d4abc9cabe171
Felix Bünemann <buenemann@louis.info>
2014-04-22 16:24:41 +0200

    Fix capturing exceptions in rspec

--- lib/pry-rescue/rspec.rb
+++ lib/pry-rescue/rspec.rb
@@ -10,9 +10,9 @@ class PryRescue
         begin
           before
 
-          example.binding.eval('@exception = nil')
+          example.binding.eval('@example.instance_variable_set(:@exception, nil)')
           example.run
-          if e = example.binding.eval('@exception')
+          if e = example.binding.eval('@example.instance_variable_get(:@exception)')
             Pry::rescued(e)
           end
 
